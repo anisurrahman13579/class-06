1a6700f0885650f06cfdd4c4c53031de26e645cb
md:Anisur rahman <anisuranisur775@gmail.com>
2024-12-15 01:19:47 +0600

    Add files via upload

---
+++ class-06/script-6.js
@@ -0,0 +1,161 @@
+/*function x()
+{
+    console.log("this is x");
+}
+x();*/
+
+
+
+// let x = ["pistol","rifle","sniper","ak-47"];
+
+
+//  x.forEach(arrayPrint);
+
+
+// function arrayPrint(value)
+
+
+// {
+//    console.log(value);
+// }
+
+ /* let r = ()=>{
+    console.log("test");
+  }
+  r();*/
+
+
+
+
+
+//    let x = ["pistol","rifle","snipar","ak-47"];
+//   const r = (v)=>{
+//     console.log(v);
+// }
+// x.forEach(r);
+
+
+
+
+
+
+// let arr = [34,64,75,68];
+// arr.map((v)=> {
+//    // console.log("hello")
+//    console.log(v*10);
+// }   )
+
+
+
+
+    //    let num = [20,10,40,10,5]
+    //    num.filter((n)=>{
+    //      console.log(n>15);           // filter er kaj holo j sortto deoya thakbe oi onujai true r felse likhe dibe , jdi oi value theke besi hy tahle true nyto felse.
+    //    }
+    
+    
+    // )
+
+   /* let x = 5;   // global value
+    function f1()
+    {
+        console.log(x);
+    }
+
+    function f2(x=10)  //parameter  // argument  f2 er jonno local varaiable.
+    {
+        console.log(x);
+    }
+    f1();
+    f2();
+    */
+       
+   /* function x ()
+    {
+        let a = "good";
+        console.log("hi, i am anisur rahamn"+a);
+    }
+    function y ()
+    {
+        let a = "good ";
+        console.log("hi, i am arif "+a);
+    }
+    x();
+    y();*/
+
+
+
+  // let s = "hello bangladesh";
+   // console.log(s.length);   // length add kor hy karon koi ta chara. ace oitar num print korar jonno.
+
+   //  console.log(s.charAt(6));
+
+    // console.log(s.indexOf("0"));
+
+     // console.log(s.substring("bang"))
+
+    // console.log(s.substring(5,8));
+
+   // console.log(s.replace("hello","hi"));
+
+       //console.log(s.trimStart());
+      // console.log(s.trimEnd()); // sesh er char ta kete dey.
+        // // console.log(s.trim());
+        // console.log (s);
+        // console.log(s.trim());
+
+      //  document.write(s);
+
+      //  alert(s);
+
+
+    //   let arr = [2,3,4,5,6,8,9];
+    //   function a (v)
+    //   {
+    //        console.log(v);
+    //   }
+    //   arr.forEach(a);   // for each functio pass korar jonno use kora hy.
+
+
+    
+    
+    /*let a = "hello world";
+    const x = a. toUpperCase();
+    console.log(x);
+    */
+  // const d = new Date ();
+//    console.log(d.getFullYear());   // getfullyear use kora hy year dekhar jonnei.
+//    console.log(d.getDate());
+//    console. log(d.getMonth());
+//    console.log(d.getTime());
+//    console.log(d.getDay());
+//    console.log(d.getHours());
+  // console.log(d.getHours()+":"+d.getMinutes()+":"+d.getSeconds());
+
+
+
+
+
+
+/*  let x = [3,4,5,6,,8];
+  function t(v)
+  {
+    console.log(v);
+  }
+  x.map(t);  // map er kaj holo array take change kore abar onno akta array te convart kora.
+*/
+function clickf()
+{
+    console.log("hello world");
+  // alert("hello world");
+}
+   
+
+
+
+
+
+
+
+         
+
